e45cf8306fba2df58b7c2803758b07a65cfe825d
renato.lucena <cpdrenato@gmail.com>
2023-09-13 20:48:30 -0300

    exemplo livewire.

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::middleware([
-//     'auth:sanctum'
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware([
+    'auth:sanctum'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 // Route::redirect('/login', '/login')->name('login');
